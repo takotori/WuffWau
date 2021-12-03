cea3c3934fa22b3120f62b0eb2af6316ac1c07f5
BuildTools <unconfigured@null.spigotmc.org>
2021-12-03 12:16:03 +0100

    implement getDoggoStats method

--- main.py
+++ main.py
@@ -8,22 +8,36 @@ def getData():
     map = {}
     for data in res["result"]["resources"]:
         map[data["title"]["de"][0:4]] = data["download_url"]
-
     response = requests.get(map[max(map.keys())]).text.splitlines()
-
-    reader = csv.DictReader(response, ["Hundename", "Geburtsjahr", "Geschlecht"])
-
-    return reader
+    return csv.DictReader(response, ["Hundename", "Geburtsjahr", "Geschlecht"])
 
 def getDoggo(doggoname):
     for doggo in getData():
         if doggo["Hundename"] == doggoname:
             print(f'{doggo["Hundename"]} {doggo["Geburtsjahr"]} {doggo["Geschlecht"]}')
 
+def getDoggoStats():
+    doggoNames = []
 
+    for doggo in getData():
+        doggoNames.append(len(doggo["Hundename"]))
 
+    highestNr = max(doggoNames)
+    lowestnr = min(doggoNames)
+
+    longname = set()
+    shortname = set()
+
+    for doggo in getData():
+        if len(doggo["Hundename"]) == highestNr:
+            longname.add(doggo["Hundename"])
+        elif len(doggo["Hundename"]) == lowestnr:
+            shortname.add(doggo["Hundename"])
+
+    print(longname)
+    print(shortname)
 
 
 
 if __name__ == '__main__':
-    getDoggo("Zorro")
+    getBeegDoggoStats()
