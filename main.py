8a2982910c9c6dce28a61c957d986414a441fb7b
BuildTools <unconfigured@null.spigotmc.org>
2021-12-03 11:44:40 +0100

    implement getData method

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-# This is a sample Python script.
+import click
+import requests
+import csv
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def getData():
+    res = requests.get("https://ckan.opendata.swiss/api/3/action/package_show?id=hundenamen-aus-dem-hundebestand-der-stadt-zurich2").json()
+
+    map = {}
+    for data in res["result"]["resources"]:
+        map[data["title"]["de"][0:4]] = data["download_url"]
+
+    response = requests.get(map[max(map.keys())]).text.splitlines()
+
+    reader = csv.DictReader(response, ["Hundename", "Geburtsjahr", "Geschlecht"])
+
+    return reader
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    getDoggo("Zorro")
